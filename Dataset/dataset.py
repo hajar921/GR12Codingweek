8c5ec73c163fa05bfa8bd5173ae6f3224746c220
hajar921 <hajar.sraouni@centrale-casablanca.ma>
2025-03-13 15:04:36 +0100

    Update dataset.py

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -9,3 +9,10 @@ import shap
 
 df=pd.read_csv(r"C:\Users\Admin\Documents\GitHub\GR12Codingweek\Dataset\ObesityDataSet_raw_and_data_sinthetic.csv")
 df.head()
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
+print(df.duplicated().sum())
+df.drop_duplicates(inplace=True)
+print(df.duplicated().sum())
