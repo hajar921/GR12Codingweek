c9d7d44ec1e25ef2d5c537c3954ac51f190f43e1
inass-semmar <inass.semmar@centrale-casablanca.ma>
2025-03-13 14:38:37 +0000

    add

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -21,3 +21,24 @@ z_scores = np.abs(zscore(df.select_dtypes(include=[np.number])))
 outliers = (z_scores > 3).sum(axis=0)
 print("Nombre d'outliers detectes par case :")
 print(outliers)
+duplicates = df[df.duplicated(keep=False)]
+
+print("Lignes dupliquées :")
+print(duplicates)
+for col in df.columns:
+    duplicate_col = df.duplicated(subset=[col], keep=False)
+    if duplicate_col.any():
+        print(f"Doublons détectés dans la colonne: {col}")
+        print(df.loc[duplicate_col, [col]])
+df_numeric = df.select_dtypes(include=['number'])
+
+
+Q1 = df_numeric.quantile(0.25)
+Q3 = df_numeric.quantile(0.75)
+IQR = Q3 - Q1
+
+
+outliers_iqr = ((df_numeric < (Q1 - 1.5 * IQR)) | (df_numeric > (Q3 + 1.5 * IQR))).sum()
+
+print("Outliers détectés par la méthode IQR :")
+print(outliers_iqr)
