4b2217ae3162134dbc9b4d891f5345d4477b4e36
Othmane MAAMAR <othmane.maamar@centrale-casablanca.ma>
2025-03-13 14:32:54 +0000

    Update dataset.py

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -6,7 +6,6 @@ from scipy.stats import zscore
 from sklearn.preprocessing import LabelEncoder
 import shap
 
-
 df=pd.read_csv(r"C:\Users\Admin\Documents\GitHub\GR12Codingweek\Dataset\ObesityDataSet_raw_and_data_sinthetic.csv")
 df.head()
 print(df.head())
@@ -16,3 +15,9 @@ print(df.isnull().sum())
 print(df.duplicated().sum())
 df.drop_duplicates(inplace=True)
 print(df.duplicated().sum())
+
+from scipy.stats import zscore 
+z_scores = np.abs(zscore(df.select_dtypes(include=[np.number])))
+outliers = (z_scores > 3).sum(axis=0)
+print("Nombre d'outliers detectes par case :")
+print(outliers)
